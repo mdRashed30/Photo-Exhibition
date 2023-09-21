58a72dccab029f8a922e003da83a957f916e41b1
Md Mamatajur Rashed <soinik.us.army@gmail.com>
2023-09-21 15:30:52 -0500

    added index.js and modefied the html file

---
+++ index.js
@@ -0,0 +1,29 @@
+const btnE1 = document.getElementById('btn');
+const errorMessageE1 = document.getElementById('errorMessage');
+const galleryE1 = document.getElementById('gallery')
+
+async function fetchImage() {
+    const inputValue = document.getElementById("input").value;
+    if (inputValue > 10 || inputValue < 1) {
+        errorMessageE1.style.display = 'block';
+        return
+    }
+    try {
+        await fetch(
+            `https://api.unsplash.com/photos?per_page=${inputValue}&page=${Math.round(Math.random() * 1000)}&client_id=ChzrpPyrs1W0UPWJeI78VOnjyEZO5JdJX2tHQJhJNTg`
+        )
+            .then((res) =>
+                res.json().then((data) => {
+                    console.log(data);
+                })
+            );
+        errorMessageE1.style.display = 'none';
+    } catch (error) {
+        errorMessageE1.style.display = 'block';
+        errorMessageE1.innerHTML = "An error happened, try it later"
+
+    }
+
+}
+
+btnE1.addEventListener('click', fetchImage);
